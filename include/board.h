18bb1b0f20002b44e503bcd7f32c850e09c5610c
Tanvir-Ahmed230 <tanviremon230@gmail.com>
2026-07-15 16:18:01 +0600

    Refactor cell type definitions to use LuxuryType for game board cells

--- include/board.h
+++ include/board.h
@@ -8,15 +8,16 @@
 
 typedef enum
 {
-    Normal,
-    DL,
-    DW,
-    TL,
-    TW
-} CellType;
+    LUXURY_NONE = 0,
+    LUXURY_DOUBLE_LETTER,
+    LUXURY_TRIPLE_LETTER,
+    LUXURY_DOUBLE_WORD,
+    LUXURY_TRIPLE_WORD
+} LuxuryType;
+
 typedef struct GameBoard
 {
-    CellType cells[BOARD_SIDE][BOARD_SIDE];
+    LuxuryType cells[BOARD_SIDE][BOARD_SIDE];
     unsigned char luxuryLayout[BOARD_SIDE][BOARD_SIDE];
     int sideSize;
 } GameBoard;
