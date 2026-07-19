17b485adeb30c0aee5c3e234f180dee02d0ec57e
Tanvir-Ahmed230 <tanviremon230@gmail.com>
2026-07-19 15:24:04 +0600

    Refactor Dictionary structure and update function signatures for word validation

--- include/word_validation.h
+++ include/word_validation.h
@@ -4,15 +4,18 @@
 #include "board.h"
 #include <stdbool.h>
 
-typedef struct PlayedWord
+typedef struct Dictionary
 {
-    char word[16];
-    int score;
-    bool isValid;
-} PlayedWord;
+    char **words; // array of addresses of individual words in RAM
+    int size;
+    int capacity;
+}Dictionary;
 
-bool is_Word_In_Dictionary(const char *word, const char *dictionary);
+bool Load_Dictionary(Dictionary *dictionary, const char *dictionary_file);
+void Free_Dictionary(Dictionary *dictionary);
+
+bool Is_Word_In_Dictionary(const char *word, const Dictionary *dictionary);
 int Calculate_Word_Score(const char *word);
-int Scan_And_Validate_Move(Tile currentGrid[BOARD_SIDE][BOARD_SIDE], Tile previousGrid[BOARD_SIDE][BOARD_SIDE], const char *dictionary);
+int Scan_And_Validate_Move(Tile current_Grid[BOARD_SIDE][BOARD_SIDE], Tile previous_Grid[BOARD_SIDE][BOARD_SIDE], const Dictionary *dictionary);
 
 #endif
