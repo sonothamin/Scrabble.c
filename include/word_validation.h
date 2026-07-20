1286ca1d57a458689cac6d7418049a98c6e9d068
Tanvir-Ahmed230 <tanviremon230@gmail.com>
2026-07-20 08:29:01 +0600

    Enhance word scoring logic and validate move scanning in game mechanics

--- include/word_validation.h
+++ include/word_validation.h
@@ -6,7 +6,7 @@
 #include "dictionary.h"
 
 bool Is_Word_In_Dictionary(const char *word, const Dictionary *dictionary);
-int Calculate_Word_Score(const char *word);
+int Calculate_Word_Score(const char *word, const Tile *placedTiles, int wordLength);
 int Scan_And_Validate_Move(Tile current_Grid[BOARD_SIDE][BOARD_SIDE], Tile previous_Grid[BOARD_SIDE][BOARD_SIDE], const Dictionary *dictionary);
 
 #endif
