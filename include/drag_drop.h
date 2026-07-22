bad48cd9f760605b29718843975e698596d93ccb
Tanvir-Ahmed230 <tanviremon230@gmail.com>
2026-07-22 15:21:43 +0600

    Add additional fields to DragNDropState for enhanced drag and drop functionality

--- include/drag_drop.h
+++ include/drag_drop.h
@@ -10,6 +10,10 @@ typedef struct DragNDropState
 {
     bool isDragging;
     int draggedTileIdx;
+    bool isFromRack;
+    int sourceGridX;
+    int sourceGridY;
+    Tile draggedTile;
 } DragNDropState;
 
 void HandleDragNDropInput(GameState *match, Rectangle boardBounds, Rectangle rackRect, float tileSize, float tileSpacing);
